1fc4846bb37c034850077128bdb30a96dcaa1716
Cor Broekhuis <cornelius.broekhuis@capgemini.com>
2024-09-09 15:15:51 +0200

    next commit

--- src/test/java/com/academy/docker/Dummy1Test.java
+++ src/test/java/com/academy/docker/Dummy1Test.java
@@ -3,7 +3,6 @@ package com.academy.docker;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Dummy1Test {
@@ -16,7 +15,7 @@ public class Dummy1Test {
 
 
         boolean b = true;
-        assertFalse( b);
+        assertTrue( b);
 
     }
 
